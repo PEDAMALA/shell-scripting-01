10b6ae8291559d5e6b7f6c5df329c61e15d9d4ce
PEDAMALA <pvssai135@gmail.com>
2025-07-28 12:23:21 +0530

    Practice or testing

---
+++ 101-Practice.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Print holiday or not
+# 1. Get the current day of the week (e.g., Monday, Tuesday, ...)
+# 2. If today is not Sunday, I have to go school
+# 3. Otherwise today is holiday
+
+
+day=$(date +%A)
+
+echo "Today is $day"
+
+# Check if today is Sunday
+if [ "$day" eq "Sunday" ]; then
+    echo "It's a holiday!"
+else
+    echo "It's a working day."
+fi
