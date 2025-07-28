9823dd13c2ee9067ca1fbcd097bba25766817b6a
PEDAMALA <pvssai135@gmail.com>
2025-07-28 12:05:40 +0530

    Practice or testing

--- 07_Special_variables.sh
+++ 07_Special_variables.sh
@@ -8,4 +8,5 @@ echo "Home directory of current user: $HOME"
 echo "Which user is running this script: $USER"
 echo "Process id of current script: $$" #it is the process id of the current one i.e sh 07_special_variables.sh
 sleep 60 &
-echo "Process id of last command in background: $!"
+sleep 200 &
+echo "Process id of last command in background in the current script only: $!"
