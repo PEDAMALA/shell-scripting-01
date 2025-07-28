c6091fdd78d48a09fec4e0b6e560a7576a3495ed
PEDAMALA <pvssai135@gmail.com>
2025-07-28 11:00:40 +0530

    Practice or testing

--- Hello_World.sh
+++ Hello_World.sh
@@ -3,6 +3,7 @@
 # This line is commented only first line with Hash is not considered as comment even if it has Hash.
 
 # 1st line with shell script shebang.
-echo -e "Hello World! \n sai_kumar kid!"
+echo -e "Hello World!\nsai_kumar kid!" #here \n is used to print in next line
+echo -e "Hello World \n sai_kumar kid!"
 echo "Hi Venkaiah Swami!"
 echo "Hi Sai_Kumar kid!"
