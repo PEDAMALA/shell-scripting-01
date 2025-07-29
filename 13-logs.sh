88d326236bde20c868b40d77a33112cafad91973
PEDAMALA <pvssai135@gmail.com>
2025-07-29 10:57:53 +0530

    latest updates

--- 13-logs.sh
+++ 13-logs.sh
@@ -10,7 +10,7 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER 
+mkdir -p $LOGS_FOLDER # be careful here, if this fails, the script will not work as expected
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
