c4ba18a74693bb9ba2a520cd0fa3b53df941e2b4
PEDAMALA <pvssai135@gmail.com>
2025-07-29 12:49:29 +0530

    latest updates

--- 15-apps-install-with-loops.sh
+++ 15-apps-install-with-loops.sh
@@ -44,7 +44,7 @@ do
         dnf install $package -y &>>$LOG_FILE_NAME
         VALIDATE $? "Installing $package"
     else
-        echo -e "$package is already $Y ... INSTALLED $N"
+        echo -e "$package is already...$Y INSTALLED $N"
     fi
 done
 
