111497628cc9cdc57d4e83e1c2dd0e1da83fc67d
PEDAMALA <pvssai135@gmail.com>
2025-07-30 11:54:20 +0530

    Be-Patient

--- expense-project-shell/mysql.sh
+++ expense-project-shell/mysql.sh
@@ -7,6 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/expense-logs"
+mkdir -p $LOGS_FOLDER # here -P means pass or skip the process if the folder already exists
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
