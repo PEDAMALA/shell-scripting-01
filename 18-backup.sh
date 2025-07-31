a1ee432bfd300c6fedf40560feabd36cfedf07a7
PEDAMALA <pvssai135@gmail.com>
2025-07-31 14:47:39 +0530

    Redefining myself

--- 18-backup.sh
+++ 18-backup.sh
@@ -45,24 +45,21 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
 if [ -n "$FILES" ] # true if there are files to zip and Here double quotes are important
 then
-    echo "Files to be backed up: $FILES" &>>$LOG_FILE_NAME
-else
-    echo -e "$R No files found older than $DAYS days in $SOURCE_DIR $N"
-    exit 0
-fi
-then
     echo "Files are: $FILES"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+    
     if [ -f "$ZIP_FILE" ]
     then
         echo -e "Successfully created zip file for files older than $DAYS"
+        
         while read -r filepath # here filepath is the variable name, you can give any name
         do
             echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
             rm -rf $filepath
             echo "Deleted file: $filepath"
         done <<< $FILES
+   
     else
         echo -e "$R Error:: $N Failed to create ZIP file "
         exit 1
